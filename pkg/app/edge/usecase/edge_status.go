5983efb3feda80dacfa0793f2517452cd23971fa
MarcusYH_Chen <yuyu@gmail.com>
2023-03-06 15:42:01 +0800

    add efge status

---
+++ pkg/app/edge/usecase/edge_status.go
@@ -0,0 +1,14 @@
+package usecase
+
+type EdgeStatus int
+
+const (
+	STATUS_FREE                   EdgeStatus = 0
+	STATUS_RESERVE_INIT           EdgeStatus = 110
+	STATUS_RESERVE_PROCESSS       EdgeStatus = 120
+	STATUS_RESERVE_XR_NOT_CONNECT EdgeStatus = 130
+	STATUS_RESERVE_XR_CONNECT     EdgeStatus = 140
+	STATUS_START_APP              EdgeStatus = 150
+	STATUS_APP_RUNNING            EdgeStatus = 160
+	STATUS_PLAYING                EdgeStatus = 170
+)
